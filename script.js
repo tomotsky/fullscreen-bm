4e3952d5fc98bc4cfc45deb903bcda24a785e4ee
Tomoki Nanasawa <tomotsky@gmail.com>
2019-07-07 21:45:12 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+javascript:(
+  function(){ 
+    document.getElementsByClassName('modal-window-inner')[0].style.width = '100%'; 
+    document.getElementsByClassName('modal-window-inner')[0].style.left = '0px'; 
+    document.getElementsByClassName('modal-close')[0].style.display = 'none'; 
+    document.getElementsByClassName('toggle-sidebar-button')[0].style.display = 'none'; 
+  })()
